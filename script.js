1692560e25496e88cc8ab24b0a0b3a4d24a2d008
Baisampayan Das <44275576+Baisampayan@users.noreply.github.com>
2024-10-28 01:48:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,120 @@
-function LocomotiveJs(){
-    gsap.registerPlugin(ScrollTrigger);
+var crsr = document.querySelector("#cursor");
+var crsrblr = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove", function (e) {
+  crsr.style.left = e.x + "px";
+  crsr.style.top = e.y + "px";
+  crsrblr.style.left = e.x - 180 + "px";
+  crsrblr.style.top = e.y - 180 + "px";
+});
 
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+document.querySelectorAll("#nav li").forEach(function (ele) {
+  ele.addEventListener("mouseenter", function () {
+    crsr.style.scale = 2;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+    crsr.style.transition = "all 0.2s ease";
+  });
+  ele.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #000";
+    crsr.style.backgroundColor = "#95c11e";
+    crsr.style.transition = "all 0.2s ease";
+  });
+});
 
-    const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true
-    });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
+gsap.to("#nav", {
+  backgroundColor: "#000000",
+  height: "100px",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers: true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
 
-    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-    });
+gsap.to("#main", {
+  backgroundColor: "#000000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers: true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 5,
+  },
+});
 
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+gsap.from("#about img, #about-text", {
+  y: 90,
+  opacity: 0,
+  duration: 1.2,
+  // ease: "power2.inOut",
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: "#about",
+    scroller: "body",
+    start: "top 70%",
+    end: "bottom 30%",
+    scrub: 1,
+  },
+});
 
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-    ScrollTrigger.refresh();
+gsap.from(".cards", {
+  scale: 0.8,
+  opacity: 0,
+  duration: 1.2,
+  // ease: "power2.inOut",
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: ".cards",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 60%",
+    scrub: 2,
+  },
+});
 
-}
-LocomotiveJs()
+gsap.from("#quote-left", {
+  y: -70,
+  x: -70,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#quote-left",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 60%",
+    scrub: 4
+  },
+});
 
-var clutter = "";
+gsap.from("#quote-right", {
+  y: 70,
+  x: 70,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#quote-right",
+    scroller: "body",
+    // markers: true,
+    start: "top 90%",
+    end: "top 85%",
+    scrub: 4
+  },
+});
 
-document.querySelector("#section-2>p").textContent.split(" ").forEach(function(e){
-    clutter += `<span> ${e} </span>`;
-    document.querySelector("#section-2>p").innerHTML = clutter;
-})
-
-gsap.to("#section-2>p>span", {
-    color: "#fff",
-    scrollTrigger:{
-        trigger:`#section-2>p>span`,
-        markers: true,
-        start: "top 65%",
-        end: "bottom 100%",
-        scroller:`#main`,
-        scrub:.5
-    },
-    stagger: 0.4
-})
+gsap.from("#course-cta h3", {
+  y: 70,
+  scrollTrigger: {
+    trigger: "#course-cta h3",
+    scroller: "body",
+    start: "top 75%",
+    end: "top 90%",
+    // markers: true,
+    scrub: 4
+  },
+});
